635ae404dc36240e5983bb9c31bc4ae85bbdcd86
markus-wi <emveyh@googlemail.com>
2015-08-28 23:55:00 +0200

    added install script

---
+++ install.sh
@@ -0,0 +1,7 @@
+sudo apt-get install ack-grep
+sudo npm install -g jshint
+rm ~/.vimrc
+rm -rf ~/.vim
+cp ./vimrc ~/.vimrc
+cp -R ./vim ~/.vim
+echo "\n\n-----------------------------\nDone! Happy coding :D"
