0dcdfba429c32658aea807f398392a79ed6bee9f
Markus Wirsing <markus.wirsing@marcapo.com>
2016-12-28 11:49:11 +0100

    added temporary fix for git submodules

--- install.sh
+++ install.sh
@@ -1,7 +1,11 @@
 sudo apt-get install ack-grep
 sudo npm install -g eslint eslint-config-defaults babel-eslint
+# todo git submodule support
+rm -rf ./vim/bundle/ale
+git clone https://github.com/w0rp/ale ./vim/bundle/ale
 rm ~/.vimrc
 rm -rf ~/.vim
 cp ./vimrc ~/.vimrc
 cp -R ./vim ~/.vim
 echo "\n\n-----------------------------\nDone! Happy coding :D"
+
